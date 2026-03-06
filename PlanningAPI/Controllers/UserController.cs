220554a51727d81feb42cea79522d0fdefdfcef0
AyushSinghSql <ayush.singh@revolvespl.com>
2026-03-06 11:08:59 -0500

    changes revert

--- PlanningAPI/Controllers/UserController.cs
+++ PlanningAPI/Controllers/UserController.cs
@@ -102,7 +102,7 @@ namespace PlanningAPI.Controllers
             });
         }
 
-        [HttpPost("loginV1AyushSingh")]
+        [HttpPost("loginV1")]
         public async Task<IActionResult> LoginV1([FromBody] LoginDto dto)
         {
             var token = await _repository.LoginAsync(dto);
